811d8c592142a56f5e3f1be8ddc0be1dbe1d6165
Alex <Alex@DESKTOP-T4COF2N>
2021-05-28 19:41:59 -0500

    commit de Alexander

---
+++ src/eordinario/Eordinario.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eordinario;
+
+/**
+ *
+ * @author Alex
+ */
+public class Eordinario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
